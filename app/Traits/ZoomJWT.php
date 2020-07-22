0059973e4014fba84ec102ac4df62fd6fde2333a
Jangwook Kim <kim.jangwook.1029@gmail.com>
2020-07-22 13:05:04 +0900

    Sample Zoom API application

---
+++ app/Traits/ZoomJWT.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Facades\Log;
+
+trait ZoomJWT
+{
+    /**
+     * Generate JWT token using environment variables for using Zoom API.
+     *
+     * @return string
+     */
+    private function generateZoomToken()
+    {
+        $key = env('ZOOM_API_KEY', '');
+        $secret = env('ZOOM_API_SECRET', '');
+        $payload = [
+            'iss' => $key,
+            'exp' => strtotime('+1 minute'),
+        ];
+        return \Firebase\JWT\JWT::encode($payload, $secret, 'HS256');
+    }
+
+    /**
+     * Retrieve Zoom API base url from environment variable.
+     *
+     * @return mixed
+     */
+    private function retrieveZoomUrl()
+    {
+        return env('ZOOM_API_URL', '');
+    }
+
+    /**
+     * Generate Request with common headers.
+     * @return \Illuminate\Http\Client\PendingRequest
+     */
+    private function zoomRequest()
+    {
+        $jwt = $this->generateZoomToken();
+        return \Illuminate\Support\Facades\Http::withHeaders([
+            'authorization' => 'Bearer ' . $jwt,
+            'content-type' => 'application/json',
+        ]);
+    }
+
+    /**
+     * Generate GET request using path and query.
+     *
+     * @param string $path
+     * @param array $query
+     * @return \Illuminate\Http\Client\Response
+     */
+    public function zoomGet(string $path, array $query = [])
+    {
+        $url = $this->retrieveZoomUrl();
+        $request = $this->zoomRequest();
+        return $request->get($url . $path, $query);
+    }
+
+    /**
+     * Generate POST request using path and request body.
+     *
+     * @param string $path
+     * @param array $body
+     * @return \Illuminate\Http\Client\Response
+     */
+    public function zoomPost(string $path, array $body = [])
+    {
+        $url = $this->retrieveZoomUrl();
+        $request = $this->zoomRequest();
+        return $request->post($url . $path, $body);
+    }
+
+    /**
+     * Generate PATCH request using path and request body.
+     *
+     * @param string $path
+     * @param array $body
+     * @return \Illuminate\Http\Client\Response
+     */
+    public function zoomPatch(string $path, array $body = [])
+    {
+        $url = $this->retrieveZoomUrl();
+        $request = $this->zoomRequest();
+        return $request->patch($url . $path, $body);
+    }
+
+    /**
+     * Generate DELETE request using path and request body.
+     *
+     * @param string $path
+     * @param array $body
+     * @return \Illuminate\Http\Client\Response
+     */
+    public function zoomDelete(string $path, array $body = [])
+    {
+        $url = $this->retrieveZoomUrl();
+        $request = $this->zoomRequest();
+        return $request->delete($url . $path, $body);
+    }
+
+    /**
+     * Generate time string of Zoom format.
+     *
+     * @param string $dateTime
+     * @return string
+     */
+    public function toZoomTimeFormat(string $dateTime)
+    {
+        try {
+            $date = new \DateTime($dateTime);
+            return $date->format('Y-m-d\TH:i:s');
+        } catch(\Exception $e) {
+            Log::error('ZoomJWT->toZoomTimeFormat : ' . $e->getMessage());
+            return '';
+        }
+    }
+
+    /**
+     * Generate unix timestamp using time string and timezone information.
+     *
+     * @param string $dateTime
+     * @param string $timezone
+     * @return int|null
+     */
+    public function toUnixTimeStamp(string $dateTime, string $timezone)
+    {
+        try {
+            $date = new \DateTime($dateTime, new \DateTimeZone($timezone));
+            return $date->getTimestamp();
+        } catch (\Exception $e) {
+            Log::error('ZoomJWT->toUnixTimeStamp : ' . $e->getMessage());
+            return null;
+        }
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/meetings', 'Zoom\MeetingController@list');
+Route::post('/meetings', 'Zoom\MeetingController@create');
+Route::get('/meetings/{id}', 'Zoom\MeetingController@get')->where('id', '[0-9]+');
+Route::patch('/meetings/{id}', 'Zoom\MeetingController@update')->where('id', '[0-9]+');
+Route::delete('/meetings/{id}', 'Zoom\MeetingController@delete')->where('id', '[0-9]+');
